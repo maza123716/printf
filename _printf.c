d7001bbcf06135bd4abfca8d02d049f68ff423ed
maza123716 <64093424+maza123716@users.noreply.github.com>
2022-04-20 16:28:54 +0300

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,43 +1,48 @@
 #include "main.h"
 /**
- * _printf - Print all this parameters
- * @format: input
+ * _printf - prints anything
+ * @format: the format string
  *
- * Description: function that prints output
- *
- * Return: The output character or num
+ * Return: number of bytes printed
  */
 int _printf(const char *format, ...)
 {
-	int x = 0, o_p = 0;
-	char *ptr = (char *) format, *output_p;
-	int (*ptr_func)(va_list, char *, int);
-	va_list vlist;
+	int sum = 0;
+	va_list ap;
+	char *p, *start;
+	params_t params = PARAMS_INIT;
 
-	if (!format)
+	va_start(ap, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	va_start(vlist, format);
-	output_p = malloc(sizeof(char) * SIZE);
-	if (!output_p)
-		return (1);
-	while (format[x])
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = (char *)format; *p; p++)
 	{
-		if (format[x] != '%')
-			output_p[o_p] = format[x], o_p++;
-		else if (s_trlen(ptr) != 1)
+		init_params(&params, ap);
+		if (*p != '%')
 		{
-			ptr_func = format_type(++ptr);
-			if (!ptr_func)
-				output_p[o_p] = format[x], o_p++;
-			else
-				o_p = ptr_func(vlist, output_p, o_p), x++;
+			sum += _putchar(*p);
+			continue;
 		}
+		start = p;
+		p++;
+		while (get_flag(p, &params)) /* while char at p is flag char */
+		{
+			p++; /* next char */
+		}
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			sum += print_from_to(start, p,
+				params.l_modifier || params.h_modifier ? p - 1 : 0);
 		else
-			o_p = -1;
-		x++, ptr++;
+			sum += get_print_func(p, ap, &params);
 	}
-	va_end(vlist);
-	write(1, output_p, o_p);
-	free(output_p);
-	return (o_p);
+	_putchar(BUF_FLUSH);
+	va_end(ap);
+	return (sum);
 }
