ee51b98825a32cf8ac4d5d34bda3297dae03a58b
maza123716 <64093424+maza123716@users.noreply.github.com>
2022-04-21 18:34:36 +0300

    main.h

--- main.h
+++ main.h
@@ -1,45 +1,24 @@
-#ifndef _MAIN_H
-#define _MAIN_H
-
-#define SIZE 2048
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdlib.h>
 #include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-#include <limits.h>
-
-int _printf(const char *format, ...);
-int (*format_type(char *s))(va_list vlist, char *output_p, int o_p);
-int print_percent(va_list vlist, char *output_p, int o_p);
-int print_char(va_list vlist, char *output_p, int o_p);
-int s_trlen(char *str);
-int print_string(va_list vlist, char *output_p, int o_p);
-int print_numbers(int n, char *output_p, int o_p);
-int print_int(va_list vlist, char *output_p, int o_p);
-int print_single_int(va_list vlist, char *output_p, int o_p);
-int print_octal(va_list vlist, char *output_p, int o_p);
-int print_unumber(va_list vlist, char *output_p, int o_p);
-int print_hexa(va_list vlist, char *output_p, int o_p);
-int print_HEXA(va_list vlist, char *output_o, int o_p);
-int print_HEXA_S(char s, char *output_p, int o_p);
-int print_S_string(va_list vlist, char *output_p, int o_p);
-int print_paddress(va_list vlist, char *output_p, int o_p);
-int print_rot13(va_list vlist, char *output_p, int o_p);
-
 /**
- * struct t_format - Struct t_format
- *
- * @character: the class
- * @func: pointer to a function
- *
- * Description: the structure for the format type
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
  */
-typedef struct t_format
+typedef struct print
 {
-	char *character;
-	int (*func)(va_list vlist, char *output_p, int o_p);
-} t_f;
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
+int _putchar(char c);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int _printf(const char *format, ...);
 
 #endif
